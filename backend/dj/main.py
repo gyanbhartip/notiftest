89743f823a6afa08aea55d6dd924ee6546cd9274
Gyan Bharti <gyanbhartip@gmail.com>
2026-04-21 02:04:00 +0530

    initialized projects

---
+++ backend/dj/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dj!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ backend/fast/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get():
+    return {"message": "Hello World"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
